27aac7dd2621d15d1906aadac05d14b1331511af
Alan Wang <y2366wan@uwaterloo.ca>
2016-11-13 00:54:01 -0500

    fixed index issues

--- template.js
+++ template.js
@@ -1,5 +1,4 @@
 //jscs:disable
-var img;  // Declare variable 'img'.
 var pop_size = 50;
 var mutation_rate = 0.01;
 var population;
@@ -7,9 +6,6 @@ var mating_pool;
 
 var pic1;
 var pic2;
-
-var counter = 0;
-//Preload the pictures
 // function preload(){
 //   pic1 = loadImage("assets/peter.jpg");
 // }
@@ -27,25 +23,23 @@ function setup() {
 }
 
 function draw() {
-  //debugger;
   // Displays the image at its actual size at point (0,0)
   image(pic1, 0, 0);
 
   //start drawing the circles
   //for (var i = 0; i < population.length; i++){
-  console.log(population[counter]);
   for (var i = 0; i < pop_size; i++) {
     clear();
-    for (var k = 0; k < population[counter].gene.length; k+=7){
+    for (var k = 0; k < population[i].gene.length; k+=7){
       //values for RGB
-      var red = population[counter].gene[k];
-      var green = population[counter].gene[k+1];
-      var blue = population[counter].gene[k+2];
-      var alpha = population[counter].gene[k+3];
+      var red = population[i].gene[k];
+      var green = population[i].gene[k+1];
+      var blue = population[i].gene[k+2];
+      var alpha = population[i].gene[k+3];
       //x, y and diameter
-      var x = population[counter].gene[k+4];
-      var y = population[counter].gene[k+5];
-      var diameter = population[counter].gene[k+6] * 2;
+      var x = population[i].gene[k+4];
+      var y = population[i].gene[k+5];
+      var diameter = population[i].gene[k+6] * 2;
 
       var c = color('rgba('+red+  ','+ green + ',' + blue + ',' + alpha + ')');
       fill(c);
@@ -53,11 +47,9 @@ function draw() {
       ellipse(x, y, diameter, diameter);
     }
     image(pic1, 0, 0);
-    console.log("y value "+ population[counter].gene[7+5]);
+    //console.log("y value "+ population[i].gene[7+5]);
     population[i].calcFitness(300, 600);
   }
-  //console.log("population array: ");
-  //console.log(population);
 
   // Mating pool
   mating_pool = new Array();
