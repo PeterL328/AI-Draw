19e9abbcc528f92422be86e2cee7cb0b7093b3d3
Alan Wang <y2366wan@uwaterloo.ca>
2016-11-12 22:28:57 -0500

    fixed logic for drawing

--- template.js
+++ template.js
@@ -7,10 +7,12 @@ var mating_pool;
 
 var pic1;
 var pic2;
+
+var counter = 0;
 //Preload the pictures
 function preload(){
-  pic1 = loadImage("assets/peter.jpg");
-  pic2 = loadImage("assets/google.jpg");
+  //pic1 = loadImage("assets/peter.jpg");
+  //pic2 = loadImage("assets/google.jpg");
 }
 /*
 //Sketch 1
@@ -48,42 +50,44 @@ function setup() {
   for (var i = 0; i < pop_size; i++) {
     population.push(new DNA());
   }
+  console.log("finished setting up");
 }
 
 function draw() {
   // debugger;
   // Displays the image at its actual size at point (0,0)
-  image(pic1, 0, 0);
-  image(pic2, 0, 300);
+  //image(pic1, 0, 0);
+  //image(pic2, 0, 300);
 
   //start drawing the circles
-  for (var i = 0; i < population.length; i++){
-    console.log(population[i]);
-    for (var k = 0; k < population[i].length; k+=7){
+  //for (var i = 0; i < population.length; i++){
+    console.log(population[counter]);
+
+    for (var k = 0; k < population[counter].gene.length; k+=7){
       //values for RGB
-      var red = population[i][k];
-      var green = population[i][k+1];
-      var blue = population[i][k+2];
+      var red = population[counter][k];
+      var green = population[counter][k+1];
+      var blue = population[counter][k+2];
       //x, y and diameter
-      var x = population[i][k+4];
-      var y = population[i][k+5];
-      var diameter = population[i][k+6] * 2;
+      var x = population[counter][k+4];
+      var y = population[counter][k+5];
+      var diameter = population[counter][k+6] * 2;
 
       var c = color('rgb(red, green, blue)');
       noStroke();
       fill(c);
 
       ellipse(x, y, diameter, diameter);
-        
+
     }
 
-  }
+  //}
 
   for (var i = 0; i < pop_size; i++) {
     population[i].calcFitness(300, 600);
   }
-  console.log("population array: ");
-  console.log(population);
+  //console.log("population array: ");
+  //console.log(population);
 
   // Mating pool
   mating_pool = new Array();
@@ -95,8 +99,8 @@ function draw() {
     }
   }
   // Reproduction
-  console.log("mating pool array: ");
-  console.log(mating_pool);
+  //console.log("mating pool array: ");
+  //console.log(mating_pool);
   for (var i = 0; i < pop_size; i++) {
     // debugger;
     var a = Math.floor(Math.random()*(mating_pool.length));
@@ -104,7 +108,7 @@ function draw() {
     var partnerA = mating_pool[a];
     var partnerB = mating_pool[b];
     if (!partnerA || !partnerB) {
-      debugger;
+      //debugger;
     }
     // Crossover
     var child = partnerA.crossover(partnerB);
@@ -113,5 +117,8 @@ function draw() {
     // Add child to population
     population[i] = child;
   }
-  console.log("exiting reproduction");
+
+
+  counter++;
+  //console.log("exiting reproduction");
 }
